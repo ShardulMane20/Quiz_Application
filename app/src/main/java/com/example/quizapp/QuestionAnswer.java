dec2b6444465f700e112fe3979e4ea3fa361b375
ShardulMane20 <shardulmane369@gmail.com>
2024-08-10 15:15:42 +0530

    Quiz_Application

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "In which city ADCET is located?",
+            "Which planet is known as the Red Planet?",
+            "Who was the captain of the Indian cricket team for the ICC T20 World Cup 2024?",
+            "Which programming language is often considered the foundation for many others?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]={
+            {"Pune" , "Mumbai" , "Ashta" , "Sangli"},
+            {"Mars" , "Venus" , "Jupiter" , "None of the above"},
+            {"Virat Kohli" , "Hardik Pandya" , "Jasprit Bumrah" , "Rohit Sharma"},
+            {"Python" , "Assembly" , "Java" , "C"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers []={
+            "Ashta" ,
+            "Mars",
+            "Rohit Sharma",
+            "Assembly",
+            "All of the above"};
+}
